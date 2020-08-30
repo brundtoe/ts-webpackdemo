d5ff45c65b5de82e14324efea9e133e83880a3a9
Jackie <brundtoe@outlook.dk>
2020-08-30 10:21:03 +0200

    overført stylesheet og indhold til index.html fra webpackdemo

--- src/page-index/main.ts
+++ src/page-index/main.ts
@@ -1,5 +1,8 @@
 import docElement from '../modules/renderElement'
 
+require('../css/main.css')
+require('./page.css')
+
 document.addEventListener('DOMContentLoaded', () => {
   docElement.render('data','<strong>Hello Webpack</strong>')
   console.log('DOMContentLoaded', 'page-index')
