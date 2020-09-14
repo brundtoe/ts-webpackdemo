ad1103de6d88efa1e6c61d73b10e06c63970d581
Jackie <brundtoe@outlook.dk>
2020-09-14 09:02:30 +0200

    komponenten ajax-callback kan nu switches on/off

--- src/page-xmlhttp/main.ts
+++ src/page-xmlhttp/main.ts
@@ -2,7 +2,7 @@ import 'bootstrap'
 import '../scss/index.scss'
 import './page.scss'
 
-import docElement from "../modules/renderElement";
+//import docElement from "../modules/renderElement";
 import './ajaxcallback'
 import  './cdalbum'
 import './ajax-component'
@@ -10,28 +10,20 @@ import './ajax-component'
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-xmlhttp')
 
+const cmdButton = <HTMLButtonElement>document.querySelector('#alert')
+cmdButton.addEventListener('click', (e: Event) => {
+    e.preventDefault()
+    const container = <HTMLElement>document.querySelector('#ajaxSection')
+    const component = document.querySelector('ajax-callback')
+    if (component) {
+        container.removeChild(component)
+    } else {
+        const ajaxComponent = document.createElement('ajax-callback')
+        container.appendChild(ajaxComponent)
+    }
+
 })
 
-const components: Array<string> = []
-
-components.forEach(component => {
-  const widget = (document.getElementById(`${component}Widget`)) as HTMLElement
-  widget.addEventListener('click', e => {
-    e.preventDefault()
-    docElement.renderHtml('resultTable','')
-    docElement.renderHtml('error','')
-    components.forEach(section => {
-      if (section !== component) {
-        const sectionElement = <HTMLElement>document.getElementById(`${section}Section`)
-        sectionElement.style.display = 'none'
-        sectionElement.setAttribute('class','hide')
-      } else {
-        const componentSection = <HTMLElement>document.getElementById(`${component}Section`)
-        componentSection.style.display = 'block'
-        componentSection.setAttribute('class','show')
-      }
-    })
-  })
 })
 
 
