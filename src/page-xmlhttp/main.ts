726d2fad11d9985abae5141e0d22d4951fe80bc9
Jackie <brundtoe@outlook.dk>
2020-09-14 09:32:50 +0200

    tilføjet formular med valg af query - udfører dog altid ajax-callback

--- src/page-xmlhttp/main.ts
+++ src/page-xmlhttp/main.ts
@@ -4,26 +4,37 @@ import './page.scss'
 
 //import docElement from "../modules/renderElement";
 import './ajaxcallback'
-import  './cdalbum'
+import './cdalbum'
 import './ajax-component'
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded', 'page-xmlhttp')
+    console.log('DOMContentLoaded', 'page-xmlhttp')
+    /**
+     const cmdButton = <HTMLButtonElement>document.querySelector('#cmdButton')
+     cmdButton.addEventListener('click', (e: Event) => {
+        e.preventDefault()
+        const container = <HTMLElement>document.querySelector('#ajaxSection')
+        const component = document.querySelector('ajax-callback')
+        if (component) {
+            container.removeChild(component)
+        } else {
+            const ajaxComponent = document.createElement('ajax-callback')
+            container.appendChild(ajaxComponent)
+        }
 
-const cmdButton = <HTMLButtonElement>document.querySelector('#alert')
-cmdButton.addEventListener('click', (e: Event) => {
-    e.preventDefault()
-    const container = <HTMLElement>document.querySelector('#ajaxSection')
-    const component = document.querySelector('ajax-callback')
-    if (component) {
-        container.removeChild(component)
-    } else {
-        const ajaxComponent = document.createElement('ajax-callback')
-        container.appendChild(ajaxComponent)
-    }
+    })
+     */
 
+    const formular = <HTMLFormElement>document.querySelector('#queries')
+
+    formular.addEventListener('submit', (e) => {
+
+        e.preventDefault()
+
+        let formular = e.target
+        //@ts-ignore
+        let formData = new FormData(formular)
+        console.log(formData.get('action' +
+            ''))
+    })
 })
-
-})
-
-
