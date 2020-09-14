375aa0ed18a66641f766276df92e171a5e712070
Jackie <brundtoe@outlook.dk>
2020-09-14 16:26:06 +0200

    omlagt page-swapi til en komponent swapi-component

--- src/page-promise/main.ts
+++ src/page-promise/main.ts
@@ -1,23 +1,10 @@
 import 'bootstrap'
 import '../scss/index.scss'
 import './page.scss'
-
-import swapi from "./fetchFile"
-import handleResponse from "./handleResponsedata"
-import handleError from "./handleResponseError"
+import './swapiComponent'
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-promise')
 })
 
-const swapiButton = <HTMLElement>document.getElementById('swapiButton')
 
-swapiButton.addEventListener('click', function (e: MouseEvent) {
-  console.log('swapiButton eventlistener')
-
-  e.preventDefault()
-
-  //@ts-ignore
-  const url: string = e.target.dataset.url
-  swapi(url, 'resultTable', handleResponse, handleError)
-})
---
+++ src/page-promise/swapiComponent.ts
@@ -0,0 +1,59 @@
+import style from '../scss/componentStyle'
+import docElement from '../modules/renderElement'
+import {SwapiTypes, FetchError} from "./swapiTypes";
+import handleResponse from "./handleResponsedata"
+import handleError from "./handleResponseError"
+
+const template: HTMLTemplateElement = document.createElement('template')
+
+template.innerHTML = `${style}
+<div>
+  <h2>Promise henter Start Wars People</h2>
+  <div id="error"></div>
+  <div class="rounded">
+      <table id="resultTable" class="table table-sm table-striped"></table>
+  </div>
+</div>`
+
+class SwapiComponent extends HTMLElement {
+
+    protected url: string
+    protected domElement: HTMLElement
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'})
+        //@ts-ignore
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+        this.url = "https://swapi.dev/api/people/9"
+        //@ts-ignore
+        this.domElement = this.shadowRoot.getElementById('resultTable')
+    }
+
+    connectedCallback() {
+        //@ts-ignore
+        window.fetch(this.url, {
+            method: 'GET',
+            cache: 'no-cache'
+        })
+            .then(res => {
+                const data: Promise<SwapiTypes> = res.json()
+                if (res.ok) {
+                    return data
+                } else {
+                    throw new Error(`Data er ikke indlæst fra ${this.url}`)
+                }
+            })
+            .then(data => {
+                console.log(data)
+                const res = handleResponse(data)
+                this.domElement.innerHTML = res
+            })
+            .catch(error => {
+                const res: string = handleError(error)
+                this.domElement.innerHTML = res
+            })
+    }
+}
+if (!window.customElements.get('swapi-component')) {
+    window.customElements.define('swapi-component',SwapiComponent)
+}
