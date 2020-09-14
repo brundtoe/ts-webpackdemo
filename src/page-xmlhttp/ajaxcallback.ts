b0cc9c23b4ea45387a6837dd42ed6833498b25fc
Jackie <brundtoe@outlook.dk>
2020-09-14 10:55:40 +0200

    ajaxcallback komponenten query aktivers når komponenten aktiveres

--- src/page-xmlhttp/ajaxcallback.ts
+++ src/page-xmlhttp/ajaxcallback.ts
@@ -5,13 +5,15 @@ console.log('modulet ajaxcallback')
 const template: HTMLTemplateElement = document.createElement('template')
 
 template.innerHTML = `${style}
-    <div>
-        <button type="button" class="btn btn-primary btn-sm" id="myButton" data-url="assets/data/ajax_info.html">XMLHttpRequest</button>
-        <table id="resultTable"></table>
-        <div id="error"></div>
-</div>`
+  <div>
+    <h2>XMLHttp request med callback</h2>
+    <table id="resultTable"></table>
+    <div id="error"></div>
+  </div>`
 
 class AjaxCallback extends HTMLElement {
+
+    protected url: string = "assets/data/ajax_info.html"
 
     constructor() {
         super()
@@ -19,36 +21,30 @@ class AjaxCallback extends HTMLElement {
         //@ts-ignore
         this.shadowRoot.appendChild(template.content.cloneNode(true))
         //@ts-ignore
-        const myButton = <HTMLButtonElement>this.shadowRoot.querySelector('#myButton')
+
+    }
+    connectedCallback() {
         //@ts-ignore
         const resultTable = <HTMLElement>this.shadowRoot.querySelector('#resultTable')
         //@ts-ignore
         const errorElement = <HTMLElement>this.shadowRoot.querySelector('#error')
-
-        myButton.addEventListener('click', function (e) {
             console.log('ajax callback eventlistener')
-            e.preventDefault();
-            let url = <string>this.getAttribute('data-url');
-            let xhr: XMLHttpRequest;
-            xhr = new XMLHttpRequest();
+            let url = this.url
+            let xhr: XMLHttpRequest
+            xhr = new XMLHttpRequest()
             xhr.onload = function () {
                 if (this.status >= 200 && this.status < 300) {
-                    callback(xhr, resultTable);
+                    resultTable.innerHTML = xhr.responseText
                 } else {
-                    myError(xhr, errorElement);
+                    errorElement.innerHTML = xhr.statusText
                 }
-            };
-            xhr.open("GET", url, true);
-            xhr.send();
-        })
-
-        function callback(xhr: XMLHttpRequest, resultTable: HTMLElement) {
-            resultTable.innerHTML = xhr.responseText
+            }
+            xhr.open("GET", url, true)
+            xhr.send()
         }
 
-        function myError(xhr: XMLHttpRequest, errorElement: HTMLElement ) {
-            errorElement.innerHTML = xhr.statusText
-        }
+    disconnectedCallback() {
+        console.log('ajax-callback er fjernet')
     }
 }
 
