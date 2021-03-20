97f0cd9bcec022f18bc2f6fd96b37f1defa0a34b
Jackie <brundtoe@outlook.dk>
2021-03-20 17:43:09 +0100

    test hvor includeShadowDom = true

---
+++ cypress/integration/webpackdemo/shadow.dom.js
@@ -0,0 +1,15 @@
+describe('bookstore wih shadow dom enabled',{"includeShadowDom": true}, () => {
+
+  beforeEach(() => {
+    cy.visit('/bookstore.html')
+  })
+
+  it('dummy', () => {
+    cy.get('[data-test=readCustomers')
+      .click()
+
+    cy.get('table.show > tbody')
+      .children('tr.row')
+      .should('have.length', 29)
+  })
+})
