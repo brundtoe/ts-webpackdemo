bce847458f2d821ed47f61edea38e0c99774ed87
Jackie <brundtoe@outlook.dk>
2021-10-18 10:38:44 +0200

    fjernet anvendelsen af jquery

--- src/page-index/index.ts
+++ src/page-index/index.ts
@@ -7,7 +7,7 @@ import './page.scss'
 document.addEventListener('DOMContentLoaded', (e) => {
   docElement.renderHtml('data','<strong>Hello Webpack from TypeScript</strong>')
   console.log('DOMContentLoaded', 'page-index')
-  $('#alert').on('click', () => {
-    alert('jQuery works!')
-  })
+//  $('#alert').on('click', () => {
+//    alert('jQuery works!')
+//  })
 })
